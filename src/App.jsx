9de28f5fefd63365ba0b0d9d6e476c2fb0f38829
Ijas Ahammed <ahammedijas.official@gmail.com>
2024-04-18 04:38:07 +0530

    All sections initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
-export default function App() {
+import Nav from './components/Nav'
+import Hero from './sections/Hero'
+import PopularProducts from './sections/PopularProducts'
+import SuperQuality from './sections/SuperQuality'
+import Services from './sections/Services'
+import SpecialOffers from './sections/SpecialOffers'
+import CustomerReview from './sections/CustomerReview'
+import Subscribe from './sections/Subscribe'
+import Footer from './sections/Footer'
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="relative">
+      <Nav/>
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero/>
+      </section>
+      <section className="padding">
+        <PopularProducts/>
+      </section>
+      <section className="padding">
+        <SuperQuality/>
+      </section>
+      <section className="padding-x py-10">
+        <Services/>
+      </section>
+      <section className="padding">
+        <SpecialOffers/>
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReview/>
+      </section>
+      <section className="padding-x sm:py-32 p-16 w-full">
+        <Subscribe/>
+      </section>
+      <section className="padding-x padding-t bg-black pb-8">
+        <Footer/>
+      </section>
+    </main>
   )
 }
+
+export default App
